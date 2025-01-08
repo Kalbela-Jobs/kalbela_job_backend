7742c1e8f82bd22c4c3f935b4ef23d4578d27a20
codewithmahadihasan <73072248+codewithmahadihasan@users.noreply.github.com>
2025-01-08 12:25:58 +0600

    update

---
+++ modules/users/education/education.js
@@ -0,0 +1,89 @@
+const { education_collection } = require("../../../collection/collections/users_activity")
+const { response_sender } = require("../../hooks/respose_sender")
+
+const upload_education = async (req, res, next) => {
+      try {
+            const data = req.body
+            if (!data.user_id) {
+                  return response_sender({
+                        res,
+                        status_code: 400,
+                        error: true,
+                        message: "No user id provided",
+                        data: null,
+                  })
+            }
+            data.created_at = new Date()
+            data.updated_at = new Date()
+            const create_data = await education_collection.insertOne(data)
+            response_sender({
+                  res,
+                  status_code: 200,
+                  error: false,
+                  message: "Create new education successful",
+                  data: create_data,
+            })
+      } catch (error) {
+            next(error)
+      }
+}
+
+const get_education = async (req, res, next) => {
+      try {
+            const user_id = req.query.user_id
+            const educations = await education_collection.find({ user_id }).sort({ created_at: -1 }).toArray();
+            response_sender({
+                  res,
+                  status_code: 200,
+                  error: false,
+                  message: "Education fetched successfully",
+                  data: educations,
+            });
+      } catch (error) {
+            next(error);
+      }
+}
+
+const delete_education = async (req, res, next) => {
+      try {
+            const { education_id } = req.query;
+            await education_collection.deleteOne({ _id: new ObjectId(education_id) });
+            response_sender({
+                  res,
+                  status_code: 200,
+                  error: false,
+                  message: "Education deleted successfully",
+            });
+      } catch (error) {
+            next(error);
+      }
+}
+
+const update_education = async (req, res, next) => {
+      try {
+            const data = req.body
+            const education_id = req.query.education_id
+            const query = { _id: new ObjectId(education_id) }
+            await education_collection.updateOne(query, {
+                  $set: { ...data }
+            })
+            const education = await education_collection.findOne(query)
+            response_sender({
+                  res,
+                  status_code: 200,
+                  error: false,
+                  message: "Education update successful",
+                  data: education,
+            })
+      } catch (error) {
+            next(error)
+      }
+
+}
+
+module.exports = {
+      upload_education,
+      get_education,
+      delete_education,
+      update_education
+}
--- modules/users/user_router.js
+++ modules/users/user_router.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const { apply_jobs } = require('./Apply_jobs/Apply_jobs');
-const { save_jobs } = require('./Save_jobs/Save_jobs');
+const { save_jobs, get_saved_jobs } = require('./Save_jobs/Save_jobs');
 const { update_profile_data } = require('./update/update_profile');
 const { upload_resume, get_resume, delete_resume } = require('./Resume/Resume');
+const { upload_education, get_education, delete_education, update_education } = require('./education/education');
+const { post_certification, get_certification, delete_certification, update_certification } = require('./certifiaction/certification');
+const { update_user_skill, get_user_skill } = require('./skills/skill');
 const router = express.Router();
 
 router.post('/apply-job', apply_jobs);
 router.post('/save-jobs', save_jobs);
+router.get('/get-saved-jobs', get_saved_jobs);
 router.put('/update-profile', update_profile_data)
 router.post('/upload-resume', upload_resume)
 router.get('/get-resume', get_resume)
 router.delete('/delete-resume', delete_resume)
+router.post('/upload-education', upload_education)
+router.get('/get-education', get_education)
+router.delete('/delete-education', delete_education)
+router.patch('/update-education', update_education)
+router.post('/upload-certification', post_certification)
+router.get('/get-certification', get_certification)
+router.delete('/delete-certification', delete_certification)
+router.patch('/update-certification', update_certification)
+router.put('/update-skill', update_user_skill)
+router.get('/get-skills', get_user_skill)
+
+
+
+
 
 
 
